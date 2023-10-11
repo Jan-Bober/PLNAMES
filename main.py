596dcf151ba3c06f7b2161ffdf479b3192301c3a
JanoBobi <116585135+JanoBobi@users.noreply.github.com>
2023-10-11 13:13:58 +0200

    BETTER
    -One variable less
    -English names of variables
    -Longer variable of shortest
    ;)

--- main.py
+++ main.py
@@ -2,27 +2,27 @@ import pandas as pd
 data = pd.read_csv("7-_WYKAZ_IMION_MĘSKICH_WG_POLA_IMIĘ_PIERWSZE_WYSTĘPUJĄCYCH_W_REJESTRZE_PESEL_Z_UWZGLĘDNIENIEM_IMION_OSÓB_ZMARŁYCH.csv")
 
 data = data.iloc[0:, 0]
-dlugosc_wszystkich = 0
-najkrotsze = ["KJHBSAKJDBAKSJDBHJABHKJBHJKSABJHKBJKHABjkhasbjbjsjedbbhsejfd"]
-najdluzsze = [""]
-ilosc_imion = 0
+print(data)
+length_of_all = 0
+shortest = ["AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"]
+longest = [""]
 
 for i in data:
-    dlugosc_wszystkich += len(i)
+    length_of_all += len(i)
 
-    if len(i) > len(najdluzsze[0]):
-        najdluzsze = [i]
+    if len(i) > len(longest[0]):
+        longest = [i]
 
-    elif len(i) == len(najdluzsze[0]):
-        najdluzsze.append(i)
+    elif len(i) == len(longest[0]):
+        longest.append(i)
 
-    if len(i) < len(najkrotsze[0]):
-        najkrotsze = [i]
+    if len(i) < len(shortest[0]):
+        shortest = [i]
 
-    elif len(i) == len(najkrotsze[0]):
-        najkrotsze.append(i)
+    elif len(i) == len(shortest[0]):
+        shortest.append(i)
 
 
-print(f'Najdluzdze imiona to:{najdluzsze}')
-print(f'Najkrotsze imiona to:{najkrotsze}')
-print(f'Srednia dlugosc wynosi{dlugosc_wszystkich/len(data)}')
+print(f'Najdluzdze imiona to:{longest}')
+print(f'Najkrotsze imiona to:{shortest}')
+print(f'Srednia dlugosc wynosi{length_of_all/len(data)}')
